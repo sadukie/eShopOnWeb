93110160fefd07539df6ebadfbebd54cb46a62c0
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2024-12-13 15:45:36 -0500

    More configuration changes

---
+++ tests/ArchitectureTests/ArchitectureTests/Core/LayerDependencyTests.cs
@@ -0,0 +1,69 @@
+﻿using ArchitectureTests.Helpers;
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ArchitectureTests.Core;
+public class LayerDependencyTests
+{
+    [Fact]
+    public void CoreShouldNotDependOnBlazorAdmin()
+    {
+        IArchRule coreShouldNotDependOnBlazorAdmin = Types()
+            .That()
+            .Are(LayerHelper.CoreLayer)
+            .Should()
+            .NotDependOnAny(LayerHelper.BlazorAdminLayer)
+            .Because("Core and Blazor Admin should be independent of each other.");
+        coreShouldNotDependOnBlazorAdmin.Check(LayerHelper.Architecture);
+    }
+
+    [Fact]
+    public void CoreShouldNotDependOnBlazorShared()
+    {
+        IArchRule coreShouldNotDependOnBlazorShared = Types()
+            .That()
+            .Are(LayerHelper.CoreLayer)
+            .Should()
+            .NotDependOnAny(LayerHelper.BlazorSharedLayer)
+            .Because("Core and Blazor Shared should be independent of each other.");
+        coreShouldNotDependOnBlazorShared.Check(LayerHelper.Architecture);
+    }
+
+    [Fact]
+    public void CoreShouldNotDependOnInfrastructure()
+    {
+        IArchRule coreShouldNotDependOnInfrastructure = Types()
+            .That()
+            .Are(LayerHelper.CoreLayer)
+            .Should()
+            .NotDependOnAny(LayerHelper.InfrastructureLayer)
+            .Because("Core and Infrastructured should be independent of each other.");
+        coreShouldNotDependOnInfrastructure.Check(LayerHelper.Architecture);
+    }
+
+    [Fact]
+    public void CoreShouldNotDependOnPublicAPI()
+    {
+        IArchRule coreShouldNotDependOnPublicAPI = Types()
+            .That()
+            .Are(LayerHelper.CoreLayer)
+            .Should()
+            .NotDependOnAny(LayerHelper.PublicAPILayer)
+            .Because("Core and API should be independent of each other.");
+        coreShouldNotDependOnPublicAPI.Check(LayerHelper.Architecture);
+    }
+
+    [Fact]
+    public void CoreShouldNotDependOnWeb()
+    {
+        IArchRule coreShouldNotDependOnWeb = Types()
+            .That()
+            .Are(LayerHelper.CoreLayer)
+            .Should()
+            .NotDependOnAny(LayerHelper.WebLayer)
+            .Because("Core and Web should be independent of each other.");
+        coreShouldNotDependOnWeb.Check(LayerHelper.Architecture);
+    }
+}
---
+++ tests/ArchitectureTests/ArchitectureTests/Core/NamingTests.cs
@@ -0,0 +1,35 @@
+﻿using ArchitectureTests.Helpers;
+using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ArchitectureTests.Core;
+public class NamingTests
+{    
+    [Fact(Skip ="Show that this can enforce naming constraints")]
+    public void AggregateRootsShouldHaveAggregateInTheName()
+    {
+        IArchRule aggregatesShouldEndInAggregate =
+            Classes()
+            .That()
+            .ImplementInterface("Microsoft.eShopWeb.ApplicationCore.Interfaces.IAggregate")
+            .Should()
+            .HaveNameEndingWith("Aggregate");
+        aggregatesShouldEndInAggregate.Check(LayerHelper.Architecture);
+    }
+
+    [Fact]
+    public void IRepositoryShouldEndWithRepository()
+    {
+        IArchRule iRepositoryShouldEndWithRepository =
+            Classes()
+            .That()
+            .AreAssignableTo("Microsoft.eShopWeb.ApplicationCore.Interfaces.IRepository*",true)
+            .Should()
+            .HaveNameEndingWith("Repository");
+        iRepositoryShouldEndWithRepository.Check(LayerHelper.Architecture);
+    }
+
+}
